5af3b9a6d2669e090a41047e039f60b1ad6964e0
Willi Schönborn <w.schoenborn@gmail.com>
2013-07-23 22:25:43 +0200

    Removed comments in tests

--- ocr/test_fuzzy.py
+++ ocr/test_fuzzy.py
@@ -1,10 +1,7 @@
 # coding=utf-8
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
-"""
+from __future__ import unicode_literals
+
 import os
 import ocr
 
