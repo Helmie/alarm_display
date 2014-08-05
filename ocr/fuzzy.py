180bcc8cfa7d665d1e5b451a956e671053b70754
Willi Schönborn <w.schoenborn@gmail.com>
2014-08-05 22:20:00 +0200

    Attempt to include bitap

--- ocr/fuzzy.py
+++ ocr/fuzzy.py
@@ -1,6 +1,130 @@
+from difflib import SequenceMatcher
+
 from fuzzywuzzy.utils import make_type_consistent
 
-from difflib import SequenceMatcher
+
+def bitap(text, pattern, loc=0):
+    """Locate the best instance of 'pattern' in 'text' near 'loc' using the
+    Bitap algorithm.
+  
+    Args:
+      text: The text to search.
+      pattern: The pattern to search for.
+      loc: The location to search around.
+  
+    Returns:
+      Best match index or -1.
+    """
+    # Python doesn't have a maxint limit, so ignore this check.
+    #if self.Match_MaxBits != 0 and len(pattern) > self.Match_MaxBits:
+    #  raise ValueError("Pattern too long for this application.")
+
+    # Initialise the alphabet.
+    s = match_alphabet(pattern)
+
+    def match_bitapScore(e, x):
+        """Compute and return the score for a match with e errors and x location.
+        Accesses loc and pattern through being a closure.
+    
+        Args:
+          e: Number of errors in match.
+          x: Location of match.
+    
+        Returns:
+          Overall score for match (0.0 = good, 1.0 = bad).
+        """
+        accuracy = float(e) / len(pattern)
+        proximity = abs(loc - x)
+        return accuracy + (proximity / 1000)
+
+    # Highest score beyond which we give up.
+    score_threshold = 1.0
+    # Is there a nearby exact match? (speedup)
+    best_loc = text.find(pattern, loc)
+    if best_loc != -1:
+        score_threshold = min(match_bitapScore(0, best_loc), score_threshold)
+        # What about in the other direction? (speedup)
+        best_loc = text.rfind(pattern, loc + len(pattern))
+        if best_loc != -1:
+            score_threshold = min(match_bitapScore(0, best_loc), score_threshold)
+
+    # Initialise the bit arrays.
+    matchmask = 1 << (len(pattern) - 1)
+    best = (0.0, -1, -1)
+
+    bin_max = len(pattern) + len(text)
+    # Empty initialization added to appease pychecker.
+    last_rd = None
+    for d in xrange(len(pattern)):
+        # Scan for the best match each iteration allows for one more error.
+        # Run a binary search to determine how far from 'loc' we can stray at
+        # this error level.
+        bin_min = 0
+        bin_mid = bin_max
+        while bin_min < bin_mid:
+            if match_bitapScore(d, loc + bin_mid) <= score_threshold:
+                bin_min = bin_mid
+            else:
+                bin_max = bin_mid
+            bin_mid = (bin_max - bin_min) // 2 + bin_min
+
+        # Use the result from this iteration as the maximum for the next.
+        bin_max = bin_mid
+        start = max(1, loc - bin_mid + 1)
+        finish = min(loc + bin_mid, len(text)) + len(pattern)
+
+        rd = [0] * (finish + 2)
+        rd[finish + 1] = (1 << d) - 1
+        for j in xrange(finish, start - 1, -1):
+            if len(text) <= j - 1:
+                # Out of range.
+                charMatch = 0
+            else:
+                charMatch = s.get(text[j - 1], 0)
+            if d == 0:  # First pass: exact match.
+                rd[j] = ((rd[j + 1] << 1) | 1) & charMatch
+            else:  # Subsequent passes: fuzzy match.
+                rd[j] = (((rd[j + 1] << 1) | 1) & charMatch) | (
+                    ((last_rd[j + 1] | last_rd[j]) << 1) | 1) | last_rd[j + 1]
+            if rd[j] & matchmask:
+                score = match_bitapScore(d, j - 1)
+                # This match will almost certainly be better than any existing match.
+                # But check anyway.
+                if score <= score_threshold:
+                    # Told you so.
+                    score_threshold = score
+                    begin = j - 1
+                    end = begin + len(pattern)
+                    best = 1.0 - score, text[begin:end], begin, end
+                    if begin > loc:
+                        # When passing loc, don't exceed our current distance from loc.
+                        start = max(1, 2 * loc - begin)
+                    else:
+                        # Already passed loc, downhill from here on in.
+                        break
+        # No hope for a (better) match at greater error levels.
+        if match_bitapScore(d + 1, loc) > score_threshold:
+            break
+        last_rd = rd
+
+    return best
+
+
+def match_alphabet(pattern):
+    """Initialise the alphabet for the Bitap algorithm.
+  
+    Args:
+      pattern: The text to encode.
+  
+    Returns:
+      Hash of character locations.
+    """
+    s = {}
+    for char in pattern:
+        s[char] = 0
+    for i in xrange(len(pattern)):
+        s[pattern[i]] |= 1 << (len(pattern) - i - 1)
+    return s
 
 
 def substring(longer, shorter):
