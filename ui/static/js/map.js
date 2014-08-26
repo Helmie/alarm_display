d70d34bf0e13c2697656e31ead8947392c65a19c
Willi Schönborn <w.schoenborn@gmail.com>
2014-08-26 20:17:25 +0200

    Restructured layout

--- ui/static/js/map.js
+++ ui/static/js/map.js
@@ -3,10 +3,10 @@
         var origin = [52.634626, 13.313749];
         var target = [52.638, 13.31374];
 
-        var bounds = L.latLngBounds([origin, target]);
-        var map = L.map('map').fitBounds(bounds);
+        var bounds = L.latLngBounds([target]);
+        var map = L.map('map').zoom;
 
-        L.tileLayer('http://{s}.tile.cloudmade.com/d4fc77ea4a63471cab2423e66626cbb6/997/256/{z}/{x}/{y}.png', {
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
         }).addTo(map);
 
